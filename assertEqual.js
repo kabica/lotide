a72aa5e7b08d6e46ac0ecbe645a9719a8872d90d
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-06 22:30:28 +0000

    assert function updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected) {
 
 
   if (actual === expected) {
-    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🛑 Assertion failed: ' + actual + '!== ' + expected);
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
   }
 
 };
