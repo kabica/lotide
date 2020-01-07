66d1f7b4b53994ab227ccf0f5268f8192a058cf3
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-07 23:03:05 +0000

    Array assertion function added

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+
+const assertArraysEqual = function(arr1, arr2) {
+  if(eqArrays(arr1, arr2)) {
+    console.log('✅ Assertion passed: Arrays are equal')
+  } else {
+    console.log('🛑 Assertion failed: arrays are different')
+  }
+};
+
+const eqArrays = function(arr1 , arr2) {
+  // Check that arrays are of equal size -- allows for either array length to be used as for-loop max index
+  if(arr1.length !== arr2.length) return false;
+
+  // Result is false by default, and will be updated only if conditions are met 
+  // Each time loop executes, result starts as false and must be updated to true 
+  let result = false;
+  for(let i = 0; i < arr1.length; i++) {
+    result = false;
+    if(arr1[i] === arr2[i]) {
+      result = true;
+    }
+
+  }
+  return result;
+
+};
+
+
+// TEST FUNCTION 
+assertArraysEqual([1,2,3] , [1,2,3]); // => should PASS
