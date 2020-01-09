dff06921b522ad6d0aee87bdfcab8cf42c65a1f2
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-09 22:42:05 +0000

    takeUntil function added

--- takeUntil.js
+++ takeUntil.js
@@ -1,18 +1,13 @@
 const takeUntil = function(array, callback) {
   const result = [];
   for(const item of array){
-    // const temp = callback(item);
-    // if(temp) return result;
-
-    // result.push(temp);
     if(callback(item)) break;
     result.push(item);
   }
-  
   return result;
 }
 
 
-const data1 = [0,1,0,0,1,0,2];
-const results1 = takeUntil(data1, x => x);
+const data1 = [0,0,0,1,0,2];
+const results1 = takeUntil(data1, x => x ? 1 : x);
 console.log(results1);
