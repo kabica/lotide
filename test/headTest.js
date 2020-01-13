4f9efd53049a5cfce74c1a0442c0e74fe5f29927
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-13 18:40:04 +0000

    headTest function now integrated with Mocha and Chai assertion

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,23 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+// TEST : HEAD
+const assert = require('chai').assert;
+const head   = require('../head');
 
-let testArr = ['A',2,3,4];
-assertEqual(head(testArr) , 'A');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'A' for ['A', 2, 3, 4]", () => {
+    assert.strictEqual(head(['A', 2, 3, 4]), 'A'); 
+  });
+
+});
+
+
+
+
+
