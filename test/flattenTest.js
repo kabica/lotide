48d759de3276663442162678c5cb72c4637ce092
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-13 21:31:33 +0000

    flatten function refactored for auto-testing

---
+++ test/flattenTest.js
@@ -0,0 +1,26 @@
+// TEST : FLATTEN
+const assert = require('chai').assert;
+const flatten   = require('../flatten');
+
+describe("#flatten", () => {
+  it("returns [1,2,3,4,5] for input [1,2,3,4,5]", () => {
+    assert.deepEqual(flatten([1,2,3,4,5]), [1,2,3,4,5]);
+  });
+
+  it("returns [1,2,3,4,5,6] for input [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
+  });
+
+  it("returns [1,2,3,4,5,6,7,8,9] for input [1, 2, [3, 4 , 5 , 6], 7, [8,9]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4 , 5 , 6], 7, [8,9]]), [1,2,3,4,5,6,7,8,9]);
+  });
+
+  
+});
+
+
+
+// console.log(flatten([1, 2, [3, 4], 5, [6]]));
+
+// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]) , [1,2,3,4,5,6] )
+// assertArraysEqual(flatten([1, 2, [3, 4 , 5 , 6], 7, [8,9]]) , [1,2,3,4,5,6,7,8,9] )
