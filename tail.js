ea65b43e47aaa2b3fd74c621608d853cd1da9ff3
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-13 16:44:18 +0000

    tail ifunction has feen refactored DRY

--- tail.js
+++ tail.js
@@ -1,21 +1,10 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+// tail.js
 
 let tail = function(array) {
   let result = array.slice(1);
   return result;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
 
-// Test Case 2: Check if original array is unmodified
+
